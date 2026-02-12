72642e4a602522f7e9c39758a86b49f670a4734e
Babanfada <apatapiti@protonmail.com>
2026-02-12 17:00:46 +0100

    @commit@

--- src/axios-instance/constant.js
+++ src/axios-instance/constant.js
@@ -1,3 +1,3 @@
-//export const baseURL = "https://api.staging.alfurqaninternational.org/api/v2";
-export const baseURL = "http://localhost:3001/api/v2";
+export const baseURL = "https://api.staging.alfurqaninternational.org/api/v2";
+//export const baseURL = "http://localhost:3001/api/v2";
 
